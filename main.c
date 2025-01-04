5c782c3281edeb5786b83b6c37bbbd1a6841bd15
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-04 13:34:23 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,4 @@
-#include <gtk/gtk.h>
-#include <gst/gst.h>
-
-int main(int argc, char *argv[]) {
-    GtkWidget *window;
-    
-    gtk_init(&argc, &argv);
-
-    gst_init(&argc, &argv);
-
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "CPlayer");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
-
-    gtk_widget_show_all(window);
-
-    gtk_main();
-
-    return 0;
-}
+#include <windows.h>
+#include <commdlg.h>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
